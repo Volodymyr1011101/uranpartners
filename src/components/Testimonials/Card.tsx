87c24aa0b805c7a784fe7da416db7ff537f9ae03
Volodymyr Voronkin <volodymyr.voronkin@Volodymyrs-MacBook-Pro.local>
2025-07-30 00:48:53 +0200

    added slider

--- src/components/Testimonials/Card.tsx
+++ src/components/Testimonials/Card.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 const Card = ({image, text, link, linkText}: {image: string, text: string, link: string, linkText: string}) => {
     return (
-        <div className="text-[#fff] bg-[#23354980] py-[22px] px-[55px] rounded-[20px] border-[#FF7700] border max-w-[560px] flex flex-col gap-[12px] text-[20px] leading-[22px] m-auto">
+        <div className="text-[#fff] bg-[#23354980] py-[22px] px-[55px] rounded-[20px] border-[#FF7700] border flex flex-col gap-[12px] text-[20px] leading-[22px] m-auto flex-grow">
             <Image  src={`/images/${image}`} alt="Card" width={224} height={77} />
-            <p>
+            <p className={'card-text'} title={text}>
                 {text}
             </p>
             <a href={link} target={`_blank`} className={`text-[#FF7700]`}>{linkText}</a>
--- src/components/swiper/SwiperComponent.tsx
+++ src/components/swiper/SwiperComponent.tsx
@@ -1,20 +1,26 @@
 'use client'
-import { Swiper, SwiperSlide } from 'swiper/react';
+import {Swiper, SwiperSlide} from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import type { Swiper as SwiperClass } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import Card from "@/components/Testimonials/Card";
+import { useState} from "react";
 const SwiperComponent = () => {
+    const [swiper, setSwiper] = useState<SwiperClass | null>(null);
     return (
         <div className="px-6 slider">
+            {/*<div className="text-white" onClick={() => swiper?.slidePrev(303)}>*/}
+            {/*    prev*/}
+            {/*</div>*/}
             <Swiper
-                modules={[Navigation, Pagination, Scrollbar, A11y]}
-                spaceBetween={50}
+                // modules={[Navigation, Pagination, Scrollbar, A11y]}
+                spaceBetween={30}
                 slidesPerView={1}
-                navigation
+                // navigation
                 pagination={{ clickable: true }}
                 scrollbar={{ draggable: true }}
-                onSwiper={(swiper) => console.log(swiper)}
+                onSwiper={(swiper) => setSwiper(swiper)}
                 onSlideChange={() => console.log('slide change')}
                 breakpoints={{
                     640: {
@@ -28,18 +34,11 @@ const SwiperComponent = () => {
                     },
                 }}
             >
-                <SwiperSlide>
-                    <Card image={'spicycasinos.png'} link={"https://spicycasinos.com"} linkText={'www.spicycasinos.com'} text={'Partnering with Uranus Partners has been nothing short of excellent. Their team is reliable, responsive, and genuinely invested in building strong, long-term relationships. The brands they promote convert well and offer real value to players, which makes our collaboration smooth and rewarding. We’re proud to work with such a trustworthy and efficient affiliate program.'} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Card image={'TypedeWhiteYes1.png'} link={"https://mblizard.ca"} linkText={'www.mblizard.ca'} text={'Our collaboration with Uran Partners has exceeded expectations. The team is proactive, results-driven, and always available. It’s rare to find such consistency and transparency.'} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Card image={'TypedeWhiteYes1.png'} link={"https://gamblizard.ca"} linkText={'www.gamblizard.ca'} text={'From day one, Uran Partners has proven to be a top-tier affiliate network. Fast payouts, honest communication, and measurable growth — exactly what we need in this competitive space.'} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Card image={'TypedeWhiteYesde.png'} link={"https://gamblizard.de"} linkText={'www.gamblizard.de'} text={'We’ve seen strong ROI since partnering with Uran Partners. Their team is knowledgeable and easy to work with, and their traffic converts exceptionally well.'} />
-                </SwiperSlide>
+                {comments.map(item => (
+                    <SwiperSlide>
+                        <Card image={item.imageRef} link={item.linkRef} linkText={item.linkText} text={item.comment} />
+                    </SwiperSlide>
+                ))}
             </Swiper>
         </div>
 
@@ -47,3 +46,31 @@ const SwiperComponent = () => {
 }
 
 export default SwiperComponent;
+
+
+const comments = [
+    {
+        imageRef: 'spicycasinos.png',
+        linkRef: 'https://spicycasinos.com',
+        linkText: 'www.spicycasinos.com',
+        comment: 'Partnering with Uranus Partners has been nothing short of excellent. Their team is reliable, responsive, and genuinely invested in building strong, long-term relationships. The brands they promote convert well and offer real value to players, which makes our collaboration smooth and rewarding. We’re proud to work with such a trustworthy and efficient affiliate program.'
+    },
+    {
+        imageRef: 'TypedeWhiteYes1.png',
+        linkRef: 'https://mblizard.ca',
+        linkText: 'www.mblizard.ca',
+        comment: 'Our collaboration with Uran Partners has exceeded expectations. The team is proactive, results-driven, and always available. It’s rare to find such consistency and transparency.'
+    },
+    {
+        imageRef: 'TypedeWhiteYes1.png',
+        linkRef: 'https://gamblizard.ca',
+        linkText: 'www.gamblizard.ca',
+        comment: 'From day one, Uran Partners has proven to be a top-tier affiliate network. Fast payouts, honest communication, and measurable growth — exactly what we need in this competitive space.'
+    },
+    {
+        imageRef: 'TypedeWhiteYesde.png',
+        linkRef: 'https://gamblizard.de',
+        linkText: 'www.gamblizard.de',
+        comment: 'We’ve seen strong ROI since partnering with Uran Partners. Their team is knowledgeable and easy to work with, and their traffic converts exceptionally well.'
+    },
+]
